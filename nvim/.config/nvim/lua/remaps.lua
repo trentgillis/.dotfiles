0e7cc1ec797f7731cd91db448b22b2a3eb3d5e35
Trentin Gillis <trentin.gillis@gmail.com>
2022-09-05 18:13:55 -0600

    Use better remaps

--- nvim/.config/nvim/lua/remaps.lua
+++ nvim/.config/nvim/lua/remaps.lua
@@ -1,23 +1,24 @@
-local keymap = vim.keymap
+local keymap = vim.keymap.set
 
 -- Fast Exit Insert Mode
-keymap.set('i', 'jk', '<ESC>')
+keymap('i', 'jk', '<ESC>')
 
 -- Do not yank with x
-keymap.set('n', 'x', '"_x')
+keymap('n', 'x', '"_x')
 
 -- New tab
-keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
+keymap('n', 'te', ':tabedit<Return>', { silent = true })
 
 -- New window
-keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true })
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
+keymap('n', 'ss', ':split<Return><C-w>w', { silent = true })
+keymap('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
 
 -- Window navigation
-keymap.set('', '<C-h>', '<C-w>h')
-keymap.set('', '<C-j>', '<C-w>j')
-keymap.set('', '<C-k>', '<C-w>k')
-keymap.set('', '<C-l>', '<C-w>l')
+keymap('', '<C-h>', '<C-w>h')
+keymap('', '<C-j>', '<C-w>j')
+keymap('', '<C-k>', '<C-w>k')
+keymap('', '<C-l>', '<C-w>l')
 
--- Fast netrw
--- keymap.set('n', 'fd', ':Ex<Return>')
+-- Buffer Navigation
+keymap('n', 'gn', ':bn<cr>')
+keymap('n', 'gp', ':bp<cr>')
