ffd60ca5aa21604447efed2ca2ca7f1ed54a6050
Trentin Gillis <trentin.gillis@gmail.com>
2022-09-08 01:06:17 -0600

    Add treesitter parsing for erb files

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,16 @@
 local status, ts = pcall(require, 'nvim-treesitter.configs')
 if (not status) then return end
 
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.erb = {
+  install_info = {
+    url = "https://github.com/tree-sitter/tree-sitter-embedded-template",
+    files = { "src/parser.c" },
+    requires_generate_from_grammar = true,
+  },
+  filetype = "erb",
+}
+
 ts.setup {
   ensure_installed = {
     'lua',
@@ -13,7 +23,7 @@ ts.setup {
     'markdown',
     'html',
     'jsdoc',
-    'embedded_template',
+    'erb',
   },
   highlight = {
     enable = true,
